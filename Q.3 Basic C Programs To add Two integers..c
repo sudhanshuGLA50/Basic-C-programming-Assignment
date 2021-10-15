b1a7770000abeff580202005dbb3fbb454184d4d
sudhanshuGLA50 <91405461+sudhanshuGLA50@users.noreply.github.com>
2021-10-15 22:02:20 +0530

    Add files via upload

---
+++ Q.3 Basic C Programs To add Two integers..c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+ int a,b,s;
+ a=10;
+ b=5;
+ s=a+b;
+ printf("%d",s); 
+
+    return 0;
+}
